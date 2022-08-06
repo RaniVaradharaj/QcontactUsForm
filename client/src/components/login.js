627c4824c96337c75773527583592a0d17c7c2e0
RaniVaradharaj <ranidvhsr31@gmail.com>
2022-08-06 15:55:08 +0530

    commit

---
+++ client/src/components/login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { ReactDOM } from "react";
+import { useState } from "react";
+
+const Login = () => {
+  return <div className="login"></div>;
+};
+
+export default Login;
